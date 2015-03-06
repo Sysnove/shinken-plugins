c3a5e9c963503bc4af3b0ea1429eaef0219443c5
Guillaume Subiron <maethor@subiron.org>
2015-03-06 17:04:03 +0100

    check_maldet.sh

---
+++ check_maldet.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+maldetsessions=/usr/local/maldetect/sess
+
+if ! [ -e $maldetsessions ] ; then
+    echo "UNKNOWN: Maldet is not installed"
+    exit 3
+fi
+
+last=$maldetsessions/session.$(<$maldetsessions/session.last)
+
+if ! [ -e $maldetsessions ] ; then
+    echo "UNKNOWN: Maldet report not found"
+    exit 3
+fi
+
+hits=$(cat $last | grep "TOTAL HITS:" | cut -d ' ' -f 3)
+
+if [ $hits -eq 0 ] ; then
+    echo "OK: No malware found."
+    exit 0
+fi
+
+if [ $hits -gt 0 ] ; then
+    echo "CRITICAL: $hits malwares found !"
+    exit 2
+fi
+
+echo "UNKNOWN: total hits = $hits"
+exit 3
