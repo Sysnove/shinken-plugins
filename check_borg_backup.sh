aa169b9aa432961c824ca57f235ae951c62e0840
Guillaume Subiron <maethor@subiron.org>
2016-05-12 22:17:17 +0200

    Return codes on check_borg_backup

--- check_borg_backup.sh
+++ check_borg_backup.sh
@@ -7,11 +7,13 @@ borg info $repository::$(date +'%Y-%m-%d') > /dev/null 2>&1
 if [ $? = 0 ]; then
     # OK
     echo "OK: Last backup is $(date +'%Y-%m-%d')"
+    return 0
 else
     borg info $repository::$(date +'%Y-%m-%d' -d "yesterday") > /dev/null 2>&1
     if [ $? = 0 ]; then
         # WARNING
         echo "WARNING: Last backup is $(date +'%Y-%m-%d' -d 'yesterday')"
+        return 1
     else
         list=$(borg list --short $repository 2>&1)
         if [ $? = 0 ]; then
@@ -19,10 +21,12 @@ else
             # Last backup is $last
             # CRITICAL
             echo "CRITICAL: Last backup is $last."
+            return 2
         else
             # Unable to connect
             # CRITICAL
             echo "CRITICAL: $list" | head -n 1
+            return 2
         fi
     fi
 fi
