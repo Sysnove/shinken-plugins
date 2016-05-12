d870ab605137a1e8c3caf9685bee9426e7252977
Guillaume Subiron <maethor@subiron.org>
2016-05-12 22:38:25 +0200

    Rewrite check_borg_backup.sh

--- check_borg_backup.sh
+++ check_borg_backup.sh
@@ -1,32 +1,30 @@
-#!/bin/sh
+#!/bin/zsh
 
 repository="backups:$(hostname)"
 
-borg info $repository::$(date +'%Y-%m-%d') > /dev/null 2>&1
+list=$(borg list --short $repository 2>&1)
+
+echo $list
 
 if [ $? = 0 ]; then
-    # OK
-    echo "OK: Last backup is $(date +'%Y-%m-%d')"
-    return 0
-else
-    borg info $repository::$(date +'%Y-%m-%d' -d "yesterday") > /dev/null 2>&1
-    if [ $? = 0 ]; then
+    if echo $list | grep -q "^$(date +'%Y-%m-%d')$"; then
+        # OK
+        echo "OK: Last backup is $(date +'%Y-%m-%d')"
+        exit 0
+    elif echo $list | grep -q "^$(date +'%Y-%m-%d' -d "yesterday")$"; then
         # WARNING
         echo "WARNING: Last backup is $(date +'%Y-%m-%d' -d 'yesterday')"
-        return 1
+        exit 1
     else
-        list=$(borg list --short $repository 2>&1)
-        if [ $? = 0 ]; then
-            last=$(echo $list | tail -n 1)
-            # Last backup is $last
-            # CRITICAL
-            echo "CRITICAL: Last backup is $last."
-            return 2
-        else
-            # Unable to connect
-            # CRITICAL
-            echo "CRITICAL: $list" | head -n 1
-            return 2
-        fi
+        last=$(echo $list | tail -n 1)
+        # Last backup is $last
+        # CRITICAL
+        echo "CRITICAL: Last backup is $last."
+        exit 2
     fi
+else
+    # Unable to connect
+    # CRITICAL
+    echo "CRITICAL: $list" | head -n 1
+    exit 2
 fi
