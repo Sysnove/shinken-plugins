e786176d73492d7916a3cbcdb41f2ed6cabc62c5
Guillaume Subiron <maethor@subiron.org>
2016-05-17 09:56:41 +0200

    Add perfdata to check_borg_backup.sh

--- check_borg_backup.sh
+++ check_borg_backup.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
+
 if [ -z $1 ]; then
     repository="backups:$(hostname)"
 else
@@ -9,19 +11,21 @@ fi
 list=$(borg list --short $repository 2>&1)
 
 if [ $? = 0 ]; then
-    if echo $list | grep -q "^$(date +'%Y-%m-%d')$"; then
-        # OK
-        echo "OK: Last backup is $(date +'%Y-%m-%d')"
+    last=$(echo $list | tail -n 1)
+    stats=$(borg info backups:subiron-srv01::2016-05-17 2> /dev/null | grep "^This archive")
+    compressed_size=$(echo "$stats" | awk '{print $5 $6}')
+    dedup_size=$(echo "$stats" | awk '{print $7 $8}')
+
+    msg="Last backup is $last ($compressed_size used). | compressed_size=$compressed_size; dedup_size=$dedup_size;"
+
+    if [[ "$last" == "$(date +'%Y-%m-%d')" ]]; then
+        echo "OK: $msg" 
         exit 0
-    elif echo $list | grep -q "^$(date +'%Y-%m-%d' -d "yesterday")$"; then
-        # WARNING
-        echo "WARNING: Last backup is $(date +'%Y-%m-%d' -d 'yesterday')"
+    elif [[ "$last" == "$(date +'%Y-%m-%d' -d "yesterday")" ]]; then
+        echo "WARNING: $msg"
         exit 1
     else
-        last=$(echo $list | tail -n 1)
-        # Last backup is $last
-        # CRITICAL
-        echo "CRITICAL: Last backup is $last."
+        echo "CRITICAL: $msg"
         exit 2
     fi
 else
