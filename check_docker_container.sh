2f487059080f8dc9c4a79823de1ae03e695d71dd
Alexis Lahouze <xals@lahouze.org>
2017-03-06 15:31:57 +0100

    Add docker containes existence.

--- check_docker_container.sh
+++ check_docker_container.sh
@@ -7,7 +7,13 @@ critical(){
     exit 2
 }
 
-/usr/bin/docker container inspect ${CONTAINER} | jq -e '.[0].State.Running' || critical "Container '${CONTAINER}' is stopped or does not exist."
+RESULT=$(/usr/bin/docker container inspect ${CONTAINER})
+
+if [ $? -ne 0 ]; then
+    critical "Container with name '${CONTAINER}' does not exist."
+fi
+
+echo ${RESULT} | jq -e '.[0].State.Running' || critical "Container '${CONTAINER}' is stopped."
 
 SINCE=/usr/bin/docker container inspect ${CONTAINER} | jq '.[0].State.StartedAt'
 
