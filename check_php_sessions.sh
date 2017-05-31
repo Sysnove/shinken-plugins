3037e37914d6a6c5ca3613125ed530f70d6ac52c
Guillaume Subiron <maethor@subiron.org>
2017-05-31 11:30:05 +0200

    Added check_php_sessions

---
+++ check_php_sessions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+total=$(find / -name 'sess_*' -ctime +15 2>/dev/null | grep -v /usr/share/man/man1/sess_id.1ssl.gz | wc -l)
+
+msg="$total PHP session files found | total=$total;;;;;"
+
+if [ $total -lt 10000 ] ; then
+    echo "OK: $msg"
+    exit 0
+else
+    echo "WARNING: $msg"
+    exit 1
+fi
