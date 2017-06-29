0cc593a227f6bde852b8845daafc493432afbdbd
Guillaume Subiron <maethor@subiron.org>
2017-06-29 17:00:13 +0200

    Added check_big_log_files.sh

---
+++ check_big_log_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+files=$(find / -size +1G \( -name "*.log" -o -name catalina.out \))
+
+if [ -z "$files" ]; then
+    echo "OK: No crazy log file found."
+    exit 0
+else
+    if [ $(echo $files | wc -l) -eq 1 ]; then
+        echo "WARNING: $files size is bigger than 1Go."
+    else
+        echo "WARNING: $(echo $files | wc -l) log files are bigger than 1Go."
+    fi
+    exit 1
+fi
