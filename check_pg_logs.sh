39dbb09da845438be51125734b6ab468cbee8227
Guillaume Subiron <maethor@subiron.org>
2017-10-31 16:08:42 +0100

    Update check_pg_logs.sh

--- check_pg_logs.sh
+++ check_pg_logs.sh
@@ -7,7 +7,7 @@ SLOW_CRITICAL=5
 MINUTES=5
 
 # Get pg version
-PG_VERSION=$(ps faux | grep postgres | grep postgresql.conf | egrep -o 'postgresql/.*/main' | cut -d '/' -f 2)
+PG_VERSION=$(ps faux | grep postgresql.conf | egrep -o 'postgresql/.*/main' | cut -d '/' -f 2)
 
 LOGFILE=/var/log/postgresql/postgresql-$PG_VERSION-main.log
 
@@ -23,19 +23,28 @@ fi
 
 # Get counters
 total=$(echo "$pgbadger" | grep '^Number of queries:' | cut -d ' ' -f 4 | sed 's/,//')
-nb_select=$(echo "$pgbadger" | grep '^SELECT:'| cut -d ' ' -f 2 | sed 's/,//')
-nb_insert=$(echo "$pgbadger" | grep '^INSERT:'| cut -d ' ' -f 2 | sed 's/,//')
-nb_update=$(echo "$pgbadger" | grep '^UPDATE:'| cut -d ' ' -f 2 | sed 's/,//')
-nb_delete=$(echo "$pgbadger" | grep '^DELETE:'| cut -d ' ' -f 2 | sed 's/,//')
-nb_others=$(echo "$pgbadger" | grep '^OTHERS:'| cut -d ' ' -f 2 | sed 's/,//')
+if [ "$total" -eq 0 ]; then
+    select_per_m=0
+    insert_per_m=0
+    update_per_m=0
+    delete_per_m=0
+    others_per_m=0
+else
+    nb_select=$(echo "$pgbadger" | grep '^SELECT:'| cut -d ' ' -f 2 | sed 's/,//')
+    nb_insert=$(echo "$pgbadger" | grep '^INSERT:'| cut -d ' ' -f 2 | sed 's/,//')
+    nb_update=$(echo "$pgbadger" | grep '^UPDATE:'| cut -d ' ' -f 2 | sed 's/,//')
+    nb_delete=$(echo "$pgbadger" | grep '^DELETE:'| cut -d ' ' -f 2 | sed 's/,//')
+    nb_others=$(echo "$pgbadger" | grep '^OTHERS:'| cut -d ' ' -f 2 | sed 's/,//')
+
+    # Convert to frequency per minute
+    select_per_m=$(echo "scale=1;$nb_select/5" | bc)
+    insert_per_m=$(echo "scale=1;$nb_insert/5" | bc)
+    update_per_m=$(echo "scale=1;$nb_update/5" | bc)
+    delete_per_m=$(echo "scale=1;$nb_delete/5" | bc)
+    others_per_m=$(echo "scale=1;$nb_others/5" | bc)
+fi
 peak=$(echo "$pgbadger" | grep '^Query peak:' | cut -d ' ' -f 3 | sed 's/,//')
 
-# Convert to frequency per minute
-select_per_m=$(echo "scale=1;$nb_select/5" | bc)
-insert_per_m=$(echo "scale=1;$nb_insert/5" | bc)
-update_per_m=$(echo "scale=1;$nb_update/5" | bc)
-delete_per_m=$(echo "scale=1;$nb_delete/5" | bc)
-others_per_m=$(echo "scale=1;$nb_others/5" | bc)
 
 # Count slow queries (more than 1000ms)
 nb_slow=$(dategrep $LOGFILE --last-minutes $MINUTES --format '%Y-%m-%d %H:%M:%S' 2>/dev/null | egrep 'duration: [0-9]{4,}\.' -o | wc -l)
