3cb99a1c93befde37f67f8a540d42a5c84123bd1
Guillaume Subiron <maethor@subiron.org>
2018-02-13 15:28:39 +0100

    Added check_fpm.sh

--- check_fpm.sh
+++ check_fpm.sh
@@ -5,6 +5,8 @@ perfdata=""
 nb_pools=0
 nb_pools_down=0
 sockets_down=""
+nb_pools_unknown=0
+pool_unknown=""
 nb_pools_max_children_reached=0
 pools_max_children_reached=""
 
@@ -22,11 +24,16 @@ for sock in $(cat /etc/php*/**/fpm/pool.d/*.conf | grep '^listen =' | cut -d '='
         sockets_down="$sockets_down $socket_name "
     fi
 
-    #pool_name=$(echo "$output" | grep '^pool:' | awk '{print $2}')
+    pool_name=$(echo "$output" | grep '^pool:' | awk '{print $2}')
     pool_listen_queue=$(echo "$output" | grep '^listen queue:' | awk '{print $3}')
     pool_idle_processes=$(echo "$output" | grep '^idle processes:' | awk '{print $3}')
     pool_active_processes=$(echo "$output" | grep '^active processes:' | awk '{print $3}')
     pool_max_children_reached=$(echo "$output" | grep '^max children reached:' | awk '{print $4}')
+
+    if [ -z "$pool_name" ]; then
+        nb_pools_unknown=$(($nb_pools_unknown + 1))
+        pool_unknown="$pool_unknown $socket_name "
+    fi
 
     perfdata="$perfdata ${socket_name}_listen_queue=${pool_listen_queue:-0} ${socket_name}_idle_procs=${pool_idle_processes:-0} ${socket_name}_active_procs=${pool_active_processes:-0}"
 
@@ -39,6 +46,11 @@ done
 if [ $nb_pools_down -gt 0 ] ; then
     echo "CRITICAL - $nb_pools_down/$nb_pools pools down ($sockets_down) | $perfdata"
     exit 2
+fi
+
+if [ $nb_pools_unknown -gt 0 ] ; then
+    echo "UNKNOWN - $nb_pools_unknown/$nb_pools pools unknown ($pool_unknown) | $perfdata"
+    exit 3
 fi
 
 if [ $nb_pools_max_children_reached -gt 0 ] ; then
