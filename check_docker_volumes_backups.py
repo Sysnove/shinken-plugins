a3a248b507c9f518d7fd636ebfa732ee0b9cc716
Yann 'Meow' Bordenave <meow@meo.wf>
2018-05-18 10:24:28 +0200

    Check docker volumes backups

---
+++ check_docker_volumes_backups.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+import os
+
+from pathlib import Path
+from datetime import datetime, timedelta
+
+# Get volumes list
+volumes = subprocess.check_output(['docker', 'volume', 'list', '-q'],
+                                  stderr=subprocess.STDOUT,
+                                  ).decode('utf-8').split('\n')
+
+# Set empty lists to gather problematic backup states
+warning_volumes = []
+critical_volumes = []
+
+# Set yesterday time
+yesterday = datetime.now() - timedelta(days=1)
+
+# Iterate on that list (the last one is '' so it is skipped)
+for volume in volumes[:-1]:
+    # Get volume backup path
+    backup = Path('/var/backups/docker/volumes/%s.tar.bz2' % volume)
+
+    # For readability sake, truncate hash names
+    volname = volume
+    if len(volname) == 64:
+        volname = volname[:8]
+
+    # If file exists
+    if backup.is_file():
+        # Backup exists, is it outdated?
+        mtime = datetime.fromtimestamp(os.path.getmtime(str(backup)))
+        if mtime < yesterday:
+            # Backup outdated, warning
+            warning_volumes.append(volname)
+    else:
+        # Backup does not exist, critical
+        critical_volumes.append(volname)
+
+# Prepare reporting
+num_volumes = len(volumes) - 1
+global_status = ''
+report = ''
+
+if critical_volumes:
+    global_status = 'CRITICAL'
+    retcode = 2
+    report = '%s volumes not backed up (%s)' % (len(critical_volumes),
+                                                ' '.join(critical_volumes))
+
+if warning_volumes:
+    if not global_status:
+        global_status = 'WARNING'
+    retcode = 1
+    report = '%s %s outdated backups (%s)' % (report,
+                                              len(warning_volumes),
+                                              ' '.join(warning_volumes))
+
+if not global_status:
+    global_status = 'OK'
+    retcode = 0
+    report = 'All %s volumes backed up' % num_volumes
+
+
+# Time to report
+print('%s: %s' % (global_status, report))
+sys.exit(retcode)
