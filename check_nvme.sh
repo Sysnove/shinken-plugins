f1e5df795977138c87f4b0ca4f56e33f7a20ddc2
Yann 'Meow' Bordenave <meow@meo.wf>
2018-05-25 11:19:02 +0200

    Fix var usage

--- check_nvme.sh
+++ check_nvme.sh
@@ -13,25 +13,25 @@ DISKS=$(lsblk -e 11,253 -dn -o NAME | grep nvme)
 CRIT=false
 MESSAGE=""
 
-NVME_CMD=sudo nvme
+NVME_CMD="sudo nvme"
 
 for DISK in $DISKS ; do
   # Check for critical_warning
-  $(NVME_CMD smart-log /dev/$DISK | awk 'FNR == 2 && $3 != 0 {exit 1}')
+  $(${NVME_CMD} smart-log /dev/$DISK | awk 'FNR == 2 && $3 != 0 {exit 1}')
   if [ $? == 1 ]; then
     CRIT=true
     MESSAGE="$MESSAGE $DISK has critical warning "
   fi
 
   # Check media_errors
-  $(NVME_CMD smart-log /dev/$DISK | awk 'FNR == 15 && $3 != 0 {exit 1}')
+  $(${NVME_CMD} smart-log /dev/$DISK | awk 'FNR == 15 && $3 != 0 {exit 1}')
   if [ $? == 1 ]; then
     CRIT=true
     MESSAGE="$MESSAGE $DISK has media errors "
   fi
 
   # Check num_err_log_entries
-  $(NVME_CMD smart-log /dev/$DISK | awk 'FNR == 16 && $3 != 0 {exit 1}')
+  $(${NVME_CMD} smart-log /dev/$DISK | awk 'FNR == 16 && $3 != 0 {exit 1}')
   if [ $? == 1 ]; then
     CRIT=true
     MESSAGE="$MESSAGE $DISK has errors logged "
