f2fe49b4f2458609c28b6b39d5b48c01524a279c
Yann 'Meow' Bordenave <meow@meo.wf>
2018-05-25 11:15:47 +0200

    Change nvme command to use sudo

--- check_nvme.sh
+++ check_nvme.sh
@@ -13,23 +13,25 @@ DISKS=$(lsblk -e 11,253 -dn -o NAME | grep nvme)
 CRIT=false
 MESSAGE=""
 
+NVME_CMD=sudo nvme
+
 for DISK in $DISKS ; do
   # Check for critical_warning
-  $(nvme smart-log /dev/$DISK | awk 'FNR == 2 && $3 != 0 {exit 1}')
+  $(NVME_CMD smart-log /dev/$DISK | awk 'FNR == 2 && $3 != 0 {exit 1}')
   if [ $? == 1 ]; then
     CRIT=true
     MESSAGE="$MESSAGE $DISK has critical warning "
   fi
 
   # Check media_errors
-  $(nvme smart-log /dev/$DISK | awk 'FNR == 15 && $3 != 0 {exit 1}')
+  $(NVME_CMD smart-log /dev/$DISK | awk 'FNR == 15 && $3 != 0 {exit 1}')
   if [ $? == 1 ]; then
     CRIT=true
     MESSAGE="$MESSAGE $DISK has media errors "
   fi
 
   # Check num_err_log_entries
-  $(nvme smart-log /dev/$DISK | awk 'FNR == 16 && $3 != 0 {exit 1}')
+  $(NVME_CMD smart-log /dev/$DISK | awk 'FNR == 16 && $3 != 0 {exit 1}')
   if [ $? == 1 ]; then
     CRIT=true
     MESSAGE="$MESSAGE $DISK has errors logged "
