3231b202d31089d759b749bea7d339db494c00f3
Guillaume Subiron <maethor@subiron.org>
2018-11-19 18:50:32 +0100

    Debug check_mounts

--- check_mounts.py
+++ check_mounts.py
@@ -3,15 +3,14 @@
 # vim: ai ts=4 sts=4 et sw=4 nu
 
 
-from __future__ import (unicode_literals, absolute_import,
-                        division, print_function)
+from __future__ import unicode_literals, absolute_import, division, print_function
 
 import subprocess
 import sys
 
 
-MOUNT_CMD = '/bin/mount'
-FSTAB_PATH = '/etc/fstab'
+MOUNT_CMD = "/bin/mount"
+FSTAB_PATH = "/etc/fstab"
 
 # nagios exit code
 STATUS_OK = 0
@@ -37,25 +36,38 @@ def main():
         fstab = f.readlines()
 
     # Compare fstab entries to mount output
-    for mount_point in [l.replace('\t', ' ').split(' ')[1] for l in fstab if not l.startswith('#') and l != '\n']:
-        if mount_point and mount_point != 'none' and mount_point != 'swap':
+    for mount_point in [
+        l.replace("\t", " ").split(" ")[1]
+        for l in fstab
+        if not l.startswith("#") and l != "\n"
+    ]:
+        if mount_point and mount_point != "none" and mount_point != "swap":
             # Try to find mount point in mount output
-            mount_line = [l for l in mount if mount_point in l.split(' ') and not l.startswith('rootfs')]
+            mount_line = [
+                l
+                for l in mount
+                if mount_point in l.split(" ")
+                and not l.startswith("rootfs")
+                and not l.startswith("systemd-1")
+            ]
             if not mount_line:
                 print("%s is not mounted" % mount_point)
                 return STATUS_ERROR
 
-            assert(len(mount_line) == 1)
+            if len(mount_line) > 1:
+                print("%s found more than one time" % mount_point)
+                return STATUS_ERROR
+
             mount_line = mount_line[0]
 
             # Check mount options
             mount_options = mount_line.split(" ")[-1][1:-1].split(",")
 
-            if 'ro' in mount_options:
+            if "ro" in mount_options:
                 print("%s is read only" % mount_point)
                 return STATUS_ERROR
 
-            if 'rw' not in mount_options:
+            if "rw" not in mount_options:
                 print("%s is not read write" % mount_point)
                 return STATUS_ERROR
 
@@ -63,6 +75,7 @@ def main():
 
     print("All mount points are OK")
     return STATUS_OK
+
 
 if __name__ == "__main__":
     sys.exit(main())
