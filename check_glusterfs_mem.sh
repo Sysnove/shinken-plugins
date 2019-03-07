4be9126bac9521a00c358aa7d653d1daecb98380
Guillaume Subiron <maethor@subiron.org>
2019-03-07 14:59:55 +0100

    check_glusterfs_mem

--- check_glusterfs_mem.sh
+++ check_glusterfs_mem.sh
@@ -4,7 +4,7 @@ pid=$(pgrep -a -x glusterfs | grep /srv/docker-data | cut -d ' ' -f 1)
 
 if [ -z "$pid" ] ; then
     echo "UNKNOWN : Unable to find glusterfs pid. Is it running?"
-    return 3
+    exit 3
 fi
 
 mem=$(cat /proc/$pid/status | grep VmRSS | awk '{print $2}')
