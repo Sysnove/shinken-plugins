8c8ec117af3e05755bb4fee42154eb2b281627dc
Guillaume Subiron <maethor@subiron.org>
2019-03-07 12:08:51 +0100

    check_glusterfs_mem

--- check_glusterfs_mem.sh
+++ check_glusterfs_mem.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-mem=$(cat /proc/$(pgrep -u root glusterfs)/status | grep VmRSS | awk '{print $2}')
+pid=$(pgrep -a -x glusterfs | grep /srv/docker-data | cut -d ' ' -f 1)
+
+if [ -z "$pid" ] ; then
+    echo "UNKNOWN : Unable to find glusterfs pid. Is it running?"
+    return 3
+fi
+
+mem=$(cat /proc/$pid)/status | grep VmRSS | awk '{print $2}')
 
 echo "GlusterFS Memory : ${mem}kB | memory=${mem}kB"
