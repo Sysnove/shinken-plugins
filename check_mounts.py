30f43cf26bfd22e004d82d9485b73249a0fe8ab0
Guillaume Subiron <maethor@subiron.org>
2019-05-17 10:41:41 +0200

    Speed up check_mounts.py

--- check_mounts.py
+++ check_mounts.py
@@ -5,12 +5,13 @@
 
 from __future__ import unicode_literals, absolute_import, division, print_function
 
-import subprocess
 import sys
+import os
+import re
 
 
-MOUNT_CMD = "/bin/mount"
-FSTAB_PATH = "/etc/fstab"
+#MOUNT_CMD = "/bin/mount"
+#FSTAB_PATH = "/etc/fstab"
 
 # nagios exit code
 STATUS_OK = 0
@@ -20,60 +21,65 @@ STATUS_UNKNOWN = 3
 
 
 def main():
+    mtab_file = "/etc/mtab"
+    if not os.path.exists(mtab_file):
+        mtab_file = "/proc/mounts"
 
-    # Get mount output
-    p = subprocess.Popen(MOUNT_CMD, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    mount, err = p.communicate()
+    with open(mtab_file) as f:
+        mtab = [x.strip() for x in f.readlines()]
 
-    if p.returncode != 0 or not mount:
-        print("MOUNT ERROR: %s" % err)
-        return STATUS_ERROR
+    mounts = [l for l in mtab if (l.startswith("/") or ":/" in l)]
 
-    mount = set(mount.splitlines())
+    ##ISPConfig
+    mounts = [
+        m
+        for m in mounts
+        if not (
+            m.split(' ')[1].startswith("/var/log/ispconfig/httpd")
+            or re.match("^/(srv|var)/www/clients/.*/log", m.split(' ')[1])
+        )
+    ]
 
     # Get fstab
-    with open(FSTAB_PATH) as f:
-        fstab = f.readlines()
+    with open('/etc/fstab') as f:
+        fstab_lines = [l.strip() for l in f.readlines()]
+        fstab_lines = [re.sub(' +', ' ', l.replace('\t', ' ')) for l in fstab_lines]
+        fstab_lines = [l for l in fstab_lines if l and (l.startswith("/") or l.startswith('UUID') or ":/" in l) and not (l.startswith('#') or l.startswith("/var/log/ispconfig/httpd"))]
+        fstab_lines = [l for l in fstab_lines if l.split(' ')[1] != 'none']
 
     # Compare fstab entries to mount output
-    for mount_point in [
-        l.replace("\t", " ").split(" ")[1]
-        for l in fstab
-        if not l.startswith("#") and l != "\n"
-    ]:
-        if mount_point and mount_point != "none" and mount_point != "swap":
-            # Try to find mount point in mount output
-            mount_line = [
-                l
-                for l in mount
-                if mount_point in l.split(" ")
-                and not l.startswith("rootfs")
-                and not l.startswith("systemd-1")
-            ]
-            if not mount_line:
-                print("%s is not mounted" % mount_point)
-                return STATUS_ERROR
+    for fstab_line in fstab_lines:
+        # Try to find mount point in mount output
+        fstab_mount_point = fstab_line.split(' ')[1]
+        mount_line = [
+            l
+            for l in mounts
+            if fstab_mount_point in l.split(" ")
+        ]
+        if not mount_line:
+            print("%s is not mounted" % fstab_mount_point)
+            return STATUS_ERROR
 
-            if len(mount_line) > 1:
-                print("%s found more than one time" % mount_point)
-                return STATUS_ERROR
+        if len(mount_line) > 1:
+            print("%s found more than one time" % fstab_mount_point)
+            return STATUS_ERROR
 
-            mount_line = mount_line[0]
+        mount_line = mount_line[0]
 
-            # Check mount options
-            mount_options = mount_line.split(" ")[-1][1:-1].split(",")
+        # Check mount options
+        mount_options = mount_line.split(" ")[3].split(",")
 
-            if "ro" in mount_options:
-                print("%s is read only" % mount_point)
-                return STATUS_ERROR
+        if "ro" in mount_options:
+            print("%s is read only" % fstab_mount_point)
+            return STATUS_ERROR
 
-            if "rw" not in mount_options:
-                print("%s is not read write" % mount_point)
-                return STATUS_ERROR
+        if "rw" not in mount_options:
+            print("%s is not read write" % fstab_mount_point)
+            return STATUS_ERROR
 
-            # :TODO:maethor:151123: test write file in partition ?
+        # :TODO:maethor:151123: test write file in partition ?
 
-    print("All mount points are OK")
+    print("All %s mount points are OK" % len(fstab_lines))
     return STATUS_OK
 
 
