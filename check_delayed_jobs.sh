1d977cd6077203c4884b7677acca27341a48d6af
Alexis Lahouze <alexis@lahouze.org>
2019-06-06 17:25:49 +0200

    Cleanup old buggy query.

--- check_delayed_jobs.sh
+++ check_delayed_jobs.sh
@@ -69,13 +69,5 @@ if [ $result -gt 0 ]; then
     exit 1
 fi
 
-result=$(sudo -u postgres psql "${DATABASE}" -A -t -c "select (select count(1) from delayed_jobs where locked_by is null and failed_at is null and created_at < now() - interval '0:02:00') > 0 and (select count(1) from delayed_jobs where locked_by is not null) = 0;")
-
-if [ "$result" != "f" ]; then
-    echo "CRITICAL - delayed_job may be stalled."
-    exit 2
-fi
-
 echo "OK - delayed_job is working well."
 exit 0
-
