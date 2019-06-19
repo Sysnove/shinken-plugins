20869574939456fa0372c6f4c63739bb0e87b4d9
Alexis Lahouze <alexis@lahouze.org>
2019-06-19 16:21:18 +0200

    Improve check_delayed-jobs to integrate perfdata.

--- check_delayed_jobs.sh
+++ check_delayed_jobs.sh
@@ -13,7 +13,40 @@ cd /
 WARN_THRESHOLD=1
 CRIT_THRESHOLD=3
 
-# Check count of waiting jobs.
+output() {
+    PERFDATA="'workers'=${PID_COUNT};;;0;${EXPECTED_WORKERS}"
+    PERFDATA="${PERFDATA} 'running'=${RUNNING};;;0;${EXPECTED_WORKERS}"
+    PERFDATA="${PERFDATA} 'zombies'=${ZOMBIES};;;0;"
+    PERFDATA="${PERFDATA} 'waiting'=${WAITING};${WARN_THRESHOLD};${CRIT_THRESHOLD};0;"
+
+    echo "$* | ${PERFDATA}"
+}
+
+unknown() {
+    output "UNKNOWN - $*"
+    exit 3
+}
+
+critical() {
+    output "CRITICAL - $*"
+    exit 2
+}
+
+warning() {
+    output "WARNING - $*"
+    exit 1
+}
+
+ok() {
+    output "OK - $*"
+    exit 0
+}
+
+# Get delayed_job PIDs.
+PID_ARRAY=$(pgrep -fa delayed_job -u mapotempo | sed -re ":a;s/([0-9]+)\\s+(delayed_job\\.[0-9]+)\\s+/'\\2 host:$(hostname) pid:\\1'/;N;s/\\n/, /;ta")
+PID_COUNT=$(pgrep -f delayed_job -u mapotempo | wc -l)
+
+# Get count of waiting jobs.
 WAITING=$(sudo -u postgres psql "${DATABASE}" -A -t <<EOF
 select count(1)
 from delayed_jobs
@@ -21,15 +54,6 @@ where locked_by is null
   and failed_at is null
 EOF
 )
-
-# Get delayed_job PIDs.
-PID_ARRAY=$(pgrep -fa delayed_job -u mapotempo | sed -re ":a;s/([0-9]+)\\s+(delayed_job\\.[0-9]+)\\s+/'\\2 host:$(hostname) pid:\\1'/;N;s/\\n/, /;ta")
-PID_COUNT=$(pgrep -f delayed_job -u mapotempo | wc -l)
-
-if [ ${PID_COUNT} -ne ${EXPECTED_WORKERS} ]; then
-    echo "CRITICAL - Found ${PID_COUNT} workers but ${EXPECTED_WORKERS} are expected."
-    exit 2
-fi
 
 # Get count of running jobs.
 RUNNING=$(sudo -u postgres psql "${DATABASE}" -A -t <<EOF
@@ -41,25 +65,7 @@ where locked_by is null
 EOF
 )
 
-if [ ${WAITING} -ge ${CRIT_THRESHOLD} ]; then
-    echo "CRITICAL - Found ${WAITING} jobs waiting."
-    exit 2
-fi
-
-if [ ${WAITING} -gt 0 ]; then
-
-    if [ ${RUNNING} -lt ${EXPECTED_WORKERS} ]; then
-        echo "CRITICAL - Found ${WAITING} jobs and only ${RUNNING} running jobs (expected ${EXPECTED_WORKERS})"
-        exit 2
-    fi
-fi
-
-if [ ${WAITING} -ge ${WARN_THRESHOLD} ]; then
-    echo "WARNING - Found ${WAITING} jobs waiting."
-    exit 1
-fi
-
-# Check zombie jobs
+# Get count of zombie jobs
 ZOMBIES=$(sudo -u postgres psql "${DATABASE}" -A -t <<EOF
 select count(1)
 from delayed_jobs
@@ -67,10 +73,27 @@ where locked_by not in (${PID_ARRAY})
 EOF
 )
 
-if [ ${ZOMBIES} -gt 0 ]; then
-    echo "WARNING - Found ${ZOMBIES} zombie jobs."
-    exit 1
+if [ ${PID_COUNT} -ne ${EXPECTED_WORKERS} ]; then
+    critical "Found ${PID_COUNT} workers but ${EXPECTED_WORKERS} are expected."
 fi
 
-echo "OK - Found ${RUNNING} jobs over ${PID_COUNT} workers and ${WAITING} waiting jobs."
+if [ ${WAITING} -ge ${CRIT_THRESHOLD} ]; then
+    critical "Found ${WAITING} jobs waiting."
+fi
+
+if [ ${WAITING} -gt 0 ]; then
+    if [ ${RUNNING} -lt ${EXPECTED_WORKERS} ]; then
+        critical "Found ${WAITING} jobs and only ${RUNNING} running jobs (expected ${EXPECTED_WORKERS})."
+    fi
+fi
+
+if [ ${WAITING} -ge ${WARN_THRESHOLD} ]; then
+    warning "Found ${WAITING} jobs waiting."
+fi
+
+if [ ${ZOMBIES} -gt 0 ]; then
+    warning "Found ${ZOMBIES} zombie jobs."
+fi
+
+ok "Found ${RUNNING} jobs over ${PID_COUNT} workers and ${WAITING} waiting jobs."
 exit 0
