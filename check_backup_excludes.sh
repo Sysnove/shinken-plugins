27b980d555221d9d572c6151923ff583e4cb2f1f
Guillaume Subiron <maethor@subiron.org>
2019-10-03 18:27:51 +0200

    Debug check_backup_excludes

--- check_backup_excludes.sh
+++ check_backup_excludes.sh
@@ -6,7 +6,7 @@ backup_excludes=$(cat /etc/backup.d/90.borg | grep '^exclude =' | awk '{print $3
 bind_mounts=$(cat /etc/fstab | grep bind | grep -v '^/var/log' | awk '{print $1}')
 
 
-if echo $backup_excludes | egrep -q $FORBIDDEN_EXCLUDES; then
+if echo "$backup_excludes" | egrep -q $FORBIDDEN_EXCLUDES; then
     echo "CRITICAL - You should not exclude $FORBIDDEN_EXCLUDES"
     exit 2
 fi
@@ -14,7 +14,7 @@ fi
 missing=""
 
 for source in $bind_mounts; do
-    if ! echo $backup_excludes | egrep -q "^$source$"; then
+    if ! echo "$backup_excludes" | egrep -q "^$source$"; then
         missing="$missing$source "
     fi
 done
