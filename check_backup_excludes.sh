5c7f5a01a5c249f63fe85ad04213b15975e6378b
Guillaume Subiron <maethor@subiron.org>
2019-10-03 17:26:52 +0200

    check_backup_excludes.sh

---
+++ check_backup_excludes.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+FORBIDDEN_EXCLUDES='^/var/(www|vmail|backups)$'
+
+backup_excludes=$(cat /etc/backup.d/90.borg | grep '^exclude =' | awk '{print $3}')
+bind_mounts=$(cat /etc/fstab | grep bind | grep -v '^/var/log' | awk '{print $1}')
+
+
+if echo $backup_excludes | grep -q $FORBIDDEN_EXCLUDES; then
+    echo "CRITICAL - You should not exclude $FORBIDDEN_EXCLUDES"
+    exit 2
+fi
+
+missing=""
+
+for source in $bind_mounts; do
+    if ! echo $backup_excludes | grep -q "^$source$"; then
+        missing="$missing$source "
+    fi
+done
+
+if [ ! -z "$missing" ]; then 
+    echo "WARNING - You should exclude following path from backups: $missing"
+    exit 1
+else
+    echo "OK - Backup excludes seems to be well configured"
+    exit 0
+fi
