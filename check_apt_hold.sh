f2dfae2aeab0b541590b5587468c87217239016d
Guillaume Subiron <maethor@subiron.org>
2019-10-24 16:13:38 +0200

    Debug check_apt_hold.sh

--- check_apt_hold.sh
+++ check_apt_hold.sh
@@ -7,8 +7,8 @@ nb_security_updates=0
 for pkg in $(apt-mark showhold); do
     nb_on_hold=$((nb_on_hold + 1))
     policy="$(apt-cache policy $pkg)"
-    installed=$(echo "$policy" | head -n 2 | tail -n 1 | cut -d ':' -f 2)
-    candidate=$(echo "$policy" | head -n 3 | tail -n 1 | cut -d ':' -f 2)
+    installed=$(echo "$policy" | head -n 2 | tail -n 1 | awk '{print $2}')
+    candidate=$(echo "$policy" | head -n 3 | tail -n 1 | awk '{print $2}')
     if [[ "$installed" != "$candidate" ]]; then
         nb_updates=$((nb_updates + 1))
         if echo "$policy" | fgrep -B 20 '***' | grep Packages | grep -q security; then
