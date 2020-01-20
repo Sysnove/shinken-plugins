973b8f53677a0db572be045a87ff2b04adc623be
Guillaume Subiron <maethor@subiron.org>
2020-01-20 14:27:58 +0100

    check_disk_wrapper.sh

---
+++ check_disk_wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This wrapper manages default exclusions on check_disk.
+# The last part $(findmnt…) adds exclusions for bind mounts.
+
+if [ -x /usr/lib/nagios/plugins/check_disk ] ; then
+    exe=/usr/lib/nagios/plugins/check_disk
+elif [ -x /usr/lib64/nagios/plugins/check_disk ] ; then
+    exe=/usr/lib64/nagios/plugins/check_disk
+else
+    exit "Could not find check_disk executable."
+    exit 3
+fi
+
+$exe /usr/lib/nagios/plugins/check_disk -A -I '^/sys' -I '^/run' -I '^/dev$' -I tmpfs -I borgfs -X tmpfs -X fuse.sshfs -X fuse.nvim -X devtmpfs -X aufs -X overlay -X overlay2 $(findmnt --raw | grep '\[' | awk '{print "-I "$1}' | xargs) $@
+exit $?
