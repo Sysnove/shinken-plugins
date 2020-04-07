eaf75bef48d967f2632d14871ae0a0c6d4c16557
Guillaume Subiron <maethor@subiron.org>
2020-04-07 16:36:15 +0200

    Added check_couchdb_replication_wrapper.sh

---
+++ check_couchdb_replication_wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Manages couchdb 1 and couchdb 2.
+
+couchdb_version=$(curl -s http://localhost:5984/ | jq -r '.version')
+
+if [ -z "$couchdb_version" ]; then
+    echo "CRITICAL - Could not retrieve CouchDB version."
+    exit 2
+fi
+
+if [ "$couchdb_version" > 1 ]; then
+    /usr/local/nagios/plugins/check_couchdb2_replications.sh -H localhost -r $1
+    exit $?
+else
+    /usr/local/nagios/plugins/check_couchdb_replications.py -r $1
+    exit $?
+fi
