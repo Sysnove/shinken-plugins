b422c84c2698ac33360679dc7aacf7fe05a61ebf
Guillaume Subiron <maethor@subiron.org>
2020-04-23 10:13:41 +0200

    Added check_conntrack.sh

---
+++ check_conntrack.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+RET_OK=0
+RET_WARN=1
+RET_CRIT=2
+RET_UNKNOWN=3
+
+WEB_WARN=5000
+ALL_WARN=10000
+
+all=$(/usr/sbin/conntrack -C 2>/dev/null)
+
+if [[ -z "$all" ]]; then
+    /usr/sbin/conntrack -C
+    exit $RET_UNKNOWN
+fi
+
+http=$(/usr/sbin/conntrack -L -p tcp --dport 443 2>/dev/null | wc -l)
+https=$(/usr/sbin/conntrack -L -p tcp --dport 443 2>/dev/null | wc -l)
+
+web=$(($http+$https))
+
+perfdata="all=${all}c;$ALL_WARN web=${web}c;$WEB_WARN"
+
+if [[ $all -gt $ALL_WARN ]]; then
+    echo "WARNING - $all connections | $perfdata"
+    exit $RET_WARN
+fi
+
+if [[ $web -gt $WEB_WARN ]]; then
+    echo "WARNING - $web web connections | $perfdata"
+    exit $RET_WARN
+fi
+
+echo "OK - $all connections, $web web connections | $perfdata"
+exit $RET_OK
+
