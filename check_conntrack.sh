759f7ea62cece5d8acd87612d9de1f7d2a28ae4f
Guillaume Subiron <maethor@subiron.org>
2020-05-12 11:43:31 +0200

    Exclude local connexions from check_conntrack.sh

--- check_conntrack.sh
+++ check_conntrack.sh
@@ -20,30 +20,32 @@ do
     esac
 done
 
-all=$(/usr/sbin/conntrack -C 2>/dev/null)
+MY_IPS=$(/sbin/ifconfig | sed -En 's/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | paste -sd "|" -)
+
+all=$(/usr/sbin/conntrack -L | awk '{print $5}' | egrep -v "src=($MY_IPS)" 2>/dev/null)
 
 if [[ -z "$all" ]]; then
     /usr/sbin/conntrack -C
     exit $RET_UNKNOWN
 fi
 
-http=$(/usr/sbin/conntrack -L -p tcp --dport 443 2>/dev/null | wc -l)
-https=$(/usr/sbin/conntrack -L -p tcp --dport 443 2>/dev/null | wc -l)
+http=$(/usr/sbin/conntrack -L -p tcp --dport 80 | awk '{print $5}' | egrep -v "src=($MY_IPS)" 2>/dev/null | wc -l)
+https=$(/usr/sbin/conntrack -L -p tcp --dport 443 | awk '{print $5}' | egrep -v "src=($MY_IPS)" 2>/dev/null | wc -l)
 
 web=$(($http+$https))
 
 perfdata="all=${all};$ALL_WARN web=${web};$WEB_WARN"
 
 if [[ $all -gt $ALL_WARN ]]; then
-    echo "WARNING - $all connections | $perfdata"
+    echo "WARNING - $all external connections | $perfdata"
     exit $RET_WARN
 fi
 
 if [[ $web -gt $WEB_WARN ]]; then
-    echo "WARNING - $web web connections | $perfdata"
+    echo "WARNING - $web external web connections | $perfdata"
     exit $RET_WARN
 fi
 
-echo "OK - $all connections, $web web connections | $perfdata"
+echo "OK - $all external connections, $web external web connections | $perfdata"
 exit $RET_OK
 
