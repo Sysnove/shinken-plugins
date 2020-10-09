28d246d70e8e67923a50168399e455b2708590f6
Guillaume Subiron <maethor@subiron.org>
2020-10-09 12:06:00 +0200

    Make check_shorewall_drop.sh compatible with ipset

--- check_shorewall_drop.sh
+++ check_shorewall_drop.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-if ! which shorewall > /dev/null 2>&1 ; then
-    echo "UNKNOWN - shorewall command not found"
-    exit 3
-fi
 
-dropped=$(shorewall show dynamic | grep DROP | wc -l)
+if which ipset > /dev/null 2>&1; then
+    dropped=$(ipset list SW_DBL4 | grep 'Number of entries:' | awk '{print $4}')
+
+    if [ -z "$dropped" ]; then
+        echo "UNKNOWN - Could not find entries in ipset list SW_DBL4"
+
+        # Always return OK
+        exit 3
+    fi
+else
+    # Without ipset, use shorewall show dynamic
+    if ! which shorewall > /dev/null 2>&1 ; then
+        echo "UNKNOWN - shorewall command not found"
+        exit 3
+    fi
+
+    dropped=$(shorewall show dynamic | grep DROP | wc -l)
+fi
 
 echo "OK - $dropped IP address(es) currently dropped by Shorewall | dropped=$dropped"
 
