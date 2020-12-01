456ff308c729fab2b0b348789b715606fc0bbb57
Guillaume Subiron <maethor@subiron.org>
2020-12-01 16:21:53 +0100

    Improve check_borg_backup with threshold

--- check_borg_backup.sh
+++ check_borg_backup.sh
@@ -6,45 +6,58 @@ export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
 export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
 
 REPOSITORY="backups:$(hostname)"
-THRESHOLD=0
+WARN=24
+CRIT=48
 
-while getopts "r:t:" option
+while getopts "r:w:c:" option
 do
     case $option in
         r)
             REPOSITORY="backups:$OPTARG"
             ;;
-        t)
-            THRESHOLD=$OPTARG
+        w)
+            WARN=$OPTARG
+            ;;
+        c)
+            CRIT=$OPTARG
             ;;
         *)
     esac
 done
 
-ok_date="$(date +'%Y-%m-%d' -d "-$THRESHOLD day")"
-warn_date="$(date +'%Y-%m-%d' -d "-$((THRESHOLD + 1)) day")"
+if [ "$WARN" -ge "$CRIT" ]; then
+    echo "CRIT should be greater than WARN"
+    exit 3
+fi
 
-if list=$(borg list --short "$REPOSITORY" 2>&1); then
-    last=$(echo "$list" | tail -n 1)
+warn_date="$(date +'%s' -d "-$WARN hour")"
+crit_date="$(date +'%s' -d "-$CRIT hour")"
+
+if list=$(borg list "$REPOSITORY" --format="{name} {time}{NEWLINE}" 2>&1); then
     count=$(echo "$list" | wc -l)
+    last=$(echo "$list" | tail -n 1)
+    last_date=$(date -d "$(echo "$last" | cut -d ' ' -f2-)" +%s)
+    last_name=$(echo "$last" | cut -d ' ' -f 1)
 
-    msg="Last backup is $last"
+    msg="Last backup is $last_name"
 
-    if [[ "$last" == "$ok_date" ]]; then
-        if [[ $count -gt $MAX_BACKUPS ]]; then
-            echo "WARNING: $count backups, please check borg prune."
+    if [ ${#last_name} -eq 10 ]; then # We need to check that we don't have a -checkpoint backup
+        if [ "$last_date" -gt "$warn_date" ]; then
+            if [[ $count -gt $MAX_BACKUPS ]]; then
+                echo "WARNING: $count backups, please check borg prune."
+                exit 1
+            else
+                echo "OK: $msg" 
+                exit 0
+            fi
+        elif [ "$last_date" -gt "$crit_date" ]; then
+            echo "WARNING: $msg"
             exit 1
-        else
-            echo "OK: $msg" 
-            exit 0
         fi
-    elif [[ "$last" == "$warn_date" ]]; then
-        echo "WARNING: $msg"
-        exit 1
-    else
-        echo "CRITICAL: $msg"
-        exit 2
     fi
+
+    echo "CRITICAL: $msg"
+    exit 2
 else
     echo "CRITICAL: $list" | head -n 1
     exit 2
