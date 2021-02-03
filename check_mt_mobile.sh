91ea7c9dd1fb6dde7ae6c6468747f96a9ade0d60
Alexis Lahouze <alexis@lahouze.org>
2021-02-03 11:54:08 +0100

    Fix perfdata.

--- check_mt_mobile.sh
+++ check_mt_mobile.sh
@@ -11,9 +11,9 @@ UNKN=3
 
 perfdata(){
     if [ -z "${AGE}" ]; then
-        printf "last_updated:Us;%d;%d;0;" "${AGE_WARN}" "${AGE_CRIT}"
+        printf "'last_updated':Us;%d;%d;0;" "${AGE_WARN}" "${AGE_CRIT}"
     else
-        printf "last_updated:%ds;%d;%d;0;" "${AGE}" "${AGE_WARN}" "${AGE_CRIT}"
+        printf "'last_updated':%ds;%d;%d;0;" "${AGE}" "${AGE_WARN}" "${AGE_CRIT}"
     fi
 }
 
