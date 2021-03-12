ae5a2edc449fa5570931c66700315501d11f7369
Guillaume Subiron <maethor@subiron.org>
2021-03-12 18:17:36 +0100

    check_unbackuped.sh

---
+++ check_unbackuped.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+includes=$(sudo cat /etc/backup.d/90.borg | grep -E '^include = /[a-z0-9]+' | cut -d ' ' -f 3 | tr '\n' '|' | sed 's/|$/\n/' | sed 's+/++g')
+
+shopt -s nullglob dotglob
+
+# shellcheck disable=SC2010
+for d in $(ls / | grep -Ev "^($includes|lost\\+found|dev|proc|sys|run|tmp)$" | grep -Ev '^(vmlinuz|initrd)'); do
+    files=(/"$d"/*)
+    if [ ${#files[@]} -gt 0 ]; then
+        echo "Unbackuped files found in /$d !"
+        exit 3
+    fi
+done
+
+echo "Everything seems to be backed up."
+exit 0
