5f724cc84b46f1dea802d6a0638fcc57b6e834a2
Guillaume Subiron <maethor@subiron.org>
2021-03-15 12:14:51 +0100

    check_unbackuped.sh

--- check_unbackuped.sh
+++ check_unbackuped.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-includes=$(sudo cat /etc/backup.d/90.borg | grep -E '^include = /[a-z0-9\.]+$' | cut -d ' ' -f 3 | tr '\n' '|' | sed 's/|$/\n/' | sed 's+/++g')
-excludes=$(sudo cat /etc/backup.d/90.borg | grep -E '^exclude = /[a-z0-9\.]+$' | cut -d ' ' -f 3 | tr '\n' '|' | sed 's/|$/\n/' | sed 's+/++g')
+root_includes=$(sudo cat /etc/backup.d/90.borg | grep -E '^include = /[a-z0-9\.]+$' | cut -d ' ' -f 3 | tr '\n' '|' | sed 's/|$/\n/' | sed 's+/++g')
+root_excludes=$(sudo cat /etc/backup.d/90.borg | grep -E '^exclude = /[a-z0-9\.]+$' | cut -d ' ' -f 3 | tr '\n' '|' | sed 's/|$/\n/' | sed 's+/++g')
+#other_excludes=$(sudo cat /etc/backup.d/90.borg | grep -E '^exclude = (sh:)?/[a-z0-9\.]/' | cut -d ' ' -f 3 | sed 's/sh://g' | tr '\n' '|' | sed 's/|$/\n/' | sed 's+/++g')
+other_excludes=$(sudo cat /etc/backup.d/90.borg | grep -E '^exclude = (sh:)?/[a-z0-9]+/' | cut -d ' ' -f 3 | sed 's/sh://' | tr '\n' '|' | sed 's/|$/\n/')
 
 shopt -s nullglob dotglob
 
 # shellcheck disable=SC2010
-for d in $(ls / | grep -Ev "^($includes|$excludes|lost\\+found|dev|proc|sys|run|tmp|clean|core|ansible-runs\.log)$" | grep -Ev '^(vmlinuz|initrd)'); do
-    if [ -n "$(find "/$d" -type f)" ]; then
+for d in $(ls / | grep -Ev "^($root_includes|$root_excludes|lost\\+found|dev|proc|sys|run|tmp|clean|core|ansible-runs\.log)$" | grep -Ev '^(vmlinuz|initrd)'); do
+if find "/$d" -type f | grep -qEv "^($other_excludes)"; then
         echo "Unbackuped files found in /$d !"
         exit 3
     fi
