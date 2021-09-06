e3548d1a7ad3554a46cd5cec2ef82149d4d2c742
Guillaume Subiron <maethor@subiron.org>
2021-09-06 17:52:40 +0200

    Custom user-agent in check_websites_security.sh

--- check_websites_security.sh
+++ check_websites_security.sh
@@ -14,15 +14,15 @@ warning () {
 
 check_website() {
     domain=$1
-    if LC_ALL=C curl --max-time 1 -sL "http://$domain/.env" | grep '(_DB|DB_|HOST|PORT|REDIS|MONGO|MYSQL|environment|ENVIRONMENT)'; then
+    if LC_ALL=C curl --max-time 1 -sL -A "Sysnove check_websites_security" "http://$domain/.env" | grep '(_DB|DB_|HOST|PORT|REDIS|MONGO|MYSQL|environment|ENVIRONMENT)'; then
         critical "http://$domain/.env is readable."
     fi
 
-    if LC_ALL=C curl --max-time 1 -sL "http://$domain/.htaccess" | grep -Eq '(Rewrite|IfModule|ErrorDocument|SetEnv|Auth(Type|Name|UserFile)|Require) '; then
+    if LC_ALL=C curl --max-time 1 -sL -A "Sysnove check_websites_security" "http://$domain/.htaccess" | grep -Eq '(Rewrite|IfModule|ErrorDocument|SetEnv|Auth(Type|Name|UserFile)|Require) '; then
         critical "http://$domain/.htaccess is readable."
     fi
 
-    if LC_ALL=C curl --max-time 1 -sL "http://$domain/.git/config" | grep -q '\[branch'; then
+    if LC_ALL=C curl --max-time 1 -sL -A "Sysnove check_websites_security" "http://$domain/.git/config" | grep -q '\[branch'; then
         critical "http://$domain/.git/config is readable."
     fi
 }
