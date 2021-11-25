5786b8ad880aff05925b623e5327ac0852a13252
Guillaume Subiron <maethor@subiron.org>
2021-11-25 12:08:48 +0100

    Allow to ignore names in check_docker_dangerous_containers

--- check_docker_dangerous_containers.sh
+++ check_docker_dangerous_containers.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "$1" == "-i" ]; then
+    ignored_names="$2"
+fi
+
 # Find docker containers based on dangerous images, like postgresql
 
 # Get containers
@@ -16,7 +20,12 @@ elif [ $ret -ne 0 ]; then
 fi
 
 # Filter out known wanted containers
-containers=$(echo "$containers" | grep -E -v "(base_mongo_proxy|registry_portus_mariadb|drone-)")
+containers=$(echo "$containers" | grep -E -v "^(base_mongo_proxy|registry_portus_mariadb|drone-)")
+
+# Filter out excludes
+count_ignored=$(echo "$containers" | grep -E "^$ignored_names\\." | wc -l)
+containers=$(echo "$containers" | grep -E -v "^$ignored_names\\.")
+
 
 # Get images
 images=$(echo "$containers" | xargs -l docker container inspect --format "{{ .Config.Image }}")
@@ -74,10 +83,14 @@ if [ -n "$count_elasticsearch" ]; then
     msg="$msg$count_elasticsearch elasticsearch, "
 fi
 
+if [ $count_ignored -gt 0 ]; then
+    msg_ignored=" ($count_ignored ignored)"
+fi
+
 if [ -n "$count" ]; then
-    echo "WARNING - $count dangerous containers running in docker (${msg::-2})"
+    echo "WARNING - $count dangerous containers running in docker (${msg::-2})$msg_ignored"
     exit 1
 fi
 
-echo "OK - no dangerous containers found in docker"
+echo "OK - no dangerous containers found in docker$msg_ignored"
 exit 0
