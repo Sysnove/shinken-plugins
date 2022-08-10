0d759c2993fd02cc35ba56efc4db9708d817eff9
Guillaume Subiron <maethor@subiron.org>
2022-08-10 15:52:11 +0200

    Improve check_fpm output

--- check_fpm.sh
+++ check_fpm.sh
@@ -7,23 +7,17 @@ nb_pools_down=0
 pools_down=""
 nb_pools_unknown=0
 pool_unknown=""
-#nb_pools_max_children_reached=0
-#pools_max_children_reached=""
 
 ret=0
 
 for sock in $(cat /etc/php*/**/fpm/pool.d/*.conf | grep '^listen =' | cut -d '=' -f 2) ; do
-    if [[ $sock =~ .*\.sock ]]; then
-        # It is a socket file
-        socket_name=${$(basename $sock)%.*}
-    else
-        # It is an IP/Port
-        config_file=$(grep -l "listen = $sock" /etc/php*/**/fpm/pool.d/*.conf)
-        socket_name=${$(basename $config_file)%.*}
-    fi
+    config_file=$(grep -l "listen = $sock" /etc/php*/**/fpm/pool.d/*.conf)
+    php_version=$(echo $config_file | cut -d '/' -f 4)
+    pool_name=${$(basename $config_file)%.*}
+    pool_fullname=php${php_version}-fpm/${pool_name}
 
     # ISPConfig
-    if [[ $socket_name = 'ispconfig' || $socket_name = 'apps' ]] ; then
+    if [[ $pool_name = 'ispconfig' || $pool_name = 'apps' ]] ; then
         continue;
     fi
 
@@ -33,33 +27,32 @@ for sock in $(cat /etc/php*/**/fpm/pool.d/*.conf | grep '^listen =' | cut -d '='
 
     if [ $? -ne 0 ] ; then
         nb_pools_down=$(($nb_pools_down + 1))
-        pools_down="$pools_down $socket_name "
+        if pgrep 'php-fpm.*'"$php_version" -a | grep -q "$pool_name"'[ ]*'; then
+            pools_down="$pools_down$pool_fullname (max_children_reached), "
+        else
+            pools_down="$pools_down$pool_fullname, "
+        fi
     fi
 
     if echo $output | grep -q "File not found"; then
         output=$(SCRIPT_NAME=/fpm_status SCRIPT_FILENAME=/fpm_status REQUEST_METHOD=GET timeout 5s cgi-fcgi -bind -connect $sock 2> /dev/null)
     fi
 
-    pool_name=$(echo "$output" | grep '^pool:' | awk '{print $2}')
-    pool_listen_queue=$(echo "$output" | grep '^listen queue:' | awk '{print $3}')
-    pool_idle_processes=$(echo "$output" | grep '^idle processes:' | awk '{print $3}')
-    pool_active_processes=$(echo "$output" | grep '^active processes:' | awk '{print $3}')
+    out_pool_name=$(echo "$output" | grep '^pool:' | awk '{print $2}')
+    out_pool_listen_queue=$(echo "$output" | grep '^listen queue:' | awk '{print $3}')
+    out_pool_idle_processes=$(echo "$output" | grep '^idle processes:' | awk '{print $3}')
+    out_pool_active_processes=$(echo "$output" | grep '^active processes:' | awk '{print $3}')
     #pool_max_children_reached=$(echo "$output" | grep '^max children reached:' | awk '{print $4}')
 
-    if [ -z "$pool_name" ]; then
+    if [ -z "$out_pool_name" ]; then
         nb_pools_unknown=$(($nb_pools_unknown + 1))
-        pool_unknown="$pool_unknown $socket_name "
+        pool_unknown="$pool_unknown$pool_fullname, "
     fi
 
     # ISPConfig
-    if ! [[ $socket_name == 'web'* ]] ; then
-        perfdata="$perfdata ${socket_name}_listen_queue=${pool_listen_queue:-0} ${socket_name}_idle_procs=${pool_idle_processes:-0} ${socket_name}_active_procs=${pool_active_processes:-0}"
+    if ! [[ $out_pool_name == 'web'* ]] ; then
+        perfdata="$perfdata php${php_version//./}_${pool_name}_listen_queue=${pool_listen_queue:-0} php${php_version//./}_${pool_name}_idle_procs=${pool_idle_processes:-0} php${php_version//./}_${pool_name}_active_procs=${pool_active_processes:-0}"
     fi
-
-    #if [[ $pool_max_children_reached > 1 ]] ; then
-        #nb_pools_max_children_reached=$(($nb_pools_max_children_reached + 1))
-        #pools_max_children_reached="$pools_max_children_reached $socket_name:$pool_max_children_reached "
-    #fi
 done
 
 if [ $nb_pools -eq 0 ] ; then
@@ -68,19 +61,14 @@ if [ $nb_pools -eq 0 ] ; then
 fi
 
 if [ $nb_pools_down -gt 0 ] ; then
-    echo "CRITICAL - $nb_pools_down/$nb_pools pools down ($pools_down) | $perfdata"
+    echo "CRITICAL - $nb_pools_down/$nb_pools pools are down or overloaded (${pools_down:0:-2}) | $perfdata"
     exit 2
 fi
 
 if [ $nb_pools_unknown -gt 0 ] ; then
-    echo "UNKNOWN - $nb_pools_unknown/$nb_pools pools unknown ($pool_unknown) | $perfdata"
+    echo "UNKNOWN - $nb_pools_unknown/$nb_pools pools are unknown (${pool_unknown:0:-2}) | $perfdata"
     exit 3
 fi
-
-#if [ $nb_pools_max_children_reached -gt 0 ] ; then
-    #echo "WARNING - $nb_pools_max_children_reached/$nb_pools pools have reached max_children ($pools_max_children_reached) | $perfdata"
-    #exit 1
-#fi
 
 echo "OK - $nb_pools PHP-FPM pools found | $perfdata"
 
