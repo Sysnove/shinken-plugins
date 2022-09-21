582344f8e54471b2b46fed376c988e4b405370c3
Guillaume Subiron <maethor@subiron.org>
2022-09-21 15:52:24 +0200

    Init check_dbms_backups.sh

---
+++ check_dbms_backups.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+databases=""
+ignored=""
+
+check_dbms_backup () {
+    dbms=$1
+
+    if ! grep -q "# ignore $dbms" /etc/backupninja.conf; then
+        ignored="$ignored $dbms"
+    else
+        if ! grep -qR "^### backupninja $dbms" /etc/backup.d; then
+            echo "CRITICAL : Missing $dbms backupninja handler"
+            exit 2
+        fi
+        backupdir=$(grep -R "^### backupninja $dbms" /etc/backup.d | cut -d ' ' -f 4)
+        if ! /usr/local/nagios/plugins/check_all_files_age.sh "$backupdir"; then
+            exit 2
+        fi
+        databases="$databases $dbms"
+    fi
+}
+
+if pgrep -f /usr/bin/mongod > /dev/null; then
+    check_dbms_backup mongodb
+fi
+
+if pgrep postgres -u postgres > /dev/null; then
+    check_dbms_backup postgres
+fi
+
+if pgrep mysql -u mysql > /dev/null; then
+    check_dbms_backup mysql
+fi
+
+if pgrep slapd > /dev/null; then
+    check_dbms_backup slapd
+fi
+
+if pgrep beam -u couchbase > /dev/null; then
+    check_dbms_backup couchbase
+fi
+
+if pgrep java -u elasticsearch > /dev/null; then
+    check_dbms_backup elasticsearch
+fi
+
+# :TODO:maethor:190109: Couchbase, couchdb… ?
+
+if [ -z "$databases" ] ; then
+    echo "OK : no database to backup on this server."
+else
+    echo "OK : $databases backups are configured in backupninja."
+fi
+exit 0
