14ade160a6aaca3341f614b7c4cde5294be4dc05
Guillaume Subiron <maethor@subiron.org>
2022-09-21 16:16:42 +0200

    check_dbms_backups.sh

--- check_dbms_backups.sh
+++ check_dbms_backups.sh
@@ -1,51 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+
+declare -A dbs=(
+    ['mongodb']="pgrep -f /usr/bin/mongod"
+    ['postgres']="pgrep postgres -u postgres"
+    ['mysql']="pgrep mysql -u mysql"
+    ['slapd']="pgrep slapd"
+    ['couchbase']="pgrep beam -u couchbase"
+    ['elasticsearch']="pgrep java -u elasticsearch"
+)
 
 databases=""
 ignored=""
 
-check_dbms_backup () {
-    dbms=$1
-
-    if ! grep -q "# ignore $dbms" /etc/backupninja.conf; then
-        ignored="$ignored $dbms"
-    else
-        if ! grep -qR "^### backupninja $dbms" /etc/backup.d; then
-            echo "CRITICAL : Missing $dbms backupninja handler"
-            exit 2
+for dbms in "${!dbs[@]}"; do
+    if eval "${dbs[$dbms]}" > /dev/null 2>&1; then
+        if ! grep -q "# ignore $dbms" /etc/backupninja.conf; then
+            ignored="$ignored $dbms"
+        else
+            if ! grep -qR "^### backupninja $dbms" /etc/backup.d; then
+                echo "CRITICAL : Missing $dbms backupninja handler"
+                exit 2
+            fi
+            backupdir=$(grep -R "^### backupninja $dbms" /etc/backup.d | cut -d ' ' -f 4)
+            if ! /usr/local/nagios/plugins/check_all_files_age.sh "$backupdir"; then
+                exit 2
+            fi
+            databases="$databases $dbms"
         fi
-        backupdir=$(grep -R "^### backupninja $dbms" /etc/backup.d | cut -d ' ' -f 4)
-        if ! /usr/local/nagios/plugins/check_all_files_age.sh "$backupdir"; then
-            exit 2
-        fi
-        databases="$databases $dbms"
     fi
-}
-
-if pgrep -f /usr/bin/mongod > /dev/null 2>&1; then
-    check_dbms_backup mongodb
-fi
-
-if pgrep postgres -u postgres > /dev/null 2>&1; then
-    check_dbms_backup postgres
-fi
-
-if pgrep mysql -u mysql > /dev/null 2>&1; then
-    check_dbms_backup mysql
-fi
-
-if pgrep slapd > /dev/null 2>&1; then
-    check_dbms_backup slapd
-fi
-
-if pgrep beam -u couchbase > /dev/null 2>&1; then
-    check_dbms_backup couchbase
-fi
-
-if pgrep java -u elasticsearch > /dev/null 2>&1; then
-    check_dbms_backup elasticsearch
-fi
-
-# :TODO:maethor:190109: Couchbase, couchdb… ?
+done
 
 if [ -z "$databases" ] ; then
     echo "OK : no database to backup on this server."
