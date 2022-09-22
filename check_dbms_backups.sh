be37fe5806a06be4ff312f9333a6a827b04972b5
Guillaume Subiron <maethor@subiron.org>
2022-09-22 13:58:40 +0200

    check_dbms_backups.sh

--- check_dbms_backups.sh
+++ check_dbms_backups.sh
@@ -13,16 +13,37 @@ declare -A dbms_checks=(
     ['elasticsearch']="pgrep java -u elasticsearch"
 )
 
+declare -A dbms_datadirs=(
+    ['mongodb']="/var/lib/mongodb"
+    ['pgsql']="/var/lib/postgresql"
+    ['mysql']="/var/lib/mysql"
+    ['ldap']="/var/lib/slapd"
+    ['couchbase']="/var/lib/couchbase"
+    ['elasticsearch']="/var/lib/elasticsearch"
+)
+
 ok=""
 ignored=""
 
 for dbms in "${!dbms_checks[@]}"; do
+    running=false
+    datadir=false
     if eval "${dbms_checks[$dbms]}" > /dev/null 2>&1; then
+        running=true
+    elif [ -d "${dbms_datadirs[$dbms]}" ]; then
+        datadir=true
+    fi
+
+    if $running || $datadir; then
         if grep -q "# ignore $dbms" /etc/backupninja.conf; then
             ignored="$ignored $dbms"
         else
             if ! grep -qR "^### backupninja $dbms" /etc/backup.d; then
-                echo "CRITICAL : Missing /etc/backup.d config for $dbms"
+                if $running; then
+                    echo "CRITICAL : $dbms is running but missing in /etc/backup.d"
+                else
+                    echo "CRITICAL : ${dbms_datadirs[$dbms]} exists but $dbms is missing in /etc/backup.d"
+                fi
                 exit 2
             fi
             backupdir=$(grep "^### backupninja $dbms" "/etc/backup.d/21_$dbms"* | cut -d ' ' -f 4)
@@ -42,6 +63,7 @@ for dbms in "${!dbms_checks[@]}"; do
     fi
 done
 
+# :TODO:maethor:20220922: Display ignored
 if [ -z "$ok" ] ; then
     echo "OK : no database to backup on this server."
 else
