1658092291a31244c293e8da8f2eee7ef49a0cc5
Guillaume Subiron <maethor@subiron.org>
2022-11-23 07:56:15 +0100

    check_varnishstat.sh

--- check_varnishstat.sh
+++ check_varnishstat.sh
@@ -22,18 +22,27 @@ if [ -f "$LAST_RUN_FILE" ] && [ ! -O "$LAST_RUN_FILE" ]; then
 fi
 
 if varnishstat=$(varnishstat -j 2>&1); then
-
-    # shellcheck disable=SC1090
-    source "$LAST_RUN_FILE"
     now=$(date +%H:%M:%S)
 
-    cache_hit=$(echo "$varnishstat" | jq '."MAIN.cache_hit".value')
-    cache_hitpass=$(echo "$varnishstat" | jq '."MAIN.cache_hitpass".value')
-    cache_miss=$(echo "$varnishstat" | jq '."MAIN.cache_miss".value')
+    varnishstat_version=$(echo "$varnishstat" | jq ".version")
+    if [ -z "$varnishstat_version" ]; then
+        cache_hit=$(echo "$varnishstat" | jq '."MAIN.cache_hit".value')
+        cache_hitpass=$(echo "$varnishstat" | jq '."MAIN.cache_hitpass".value')
+        cache_miss=$(echo "$varnishstat" | jq '."MAIN.cache_miss".value')
+    elif [ "$varnishstat_version" -eq 1 ]; then
+        cache_hit=$(echo "$varnishstat" | jq '.counters."MAIN.cache_hit".value')
+        cache_hitpass=$(echo "$varnishstat" | jq '.counters."MAIN.cache_hitpass".value')
+        cache_miss=$(echo "$varnishstat" | jq '.counters."MAIN.cache_miss".value')
+    else
+        echo "UNNOWN - Varnishstat version $varnishstat_version is not managed."
+    fi
 
     old_cache_hit=-1
     old_cache_hitpass=-1
     old_cache_miss=-1
+    # shellcheck disable=SC1090
+    source "$LAST_RUN_FILE"
+
     echo "
 old_cache_hit=$cache_hit
 old_cache_hitpass=$cache_hitpass
@@ -41,7 +50,7 @@ old_cache_miss=$cache_miss
 last_check=$now
 " > "$LAST_RUN_FILE"
 
-    if [ -z "$last_check" ]; then
+    if [ -z "$last_check" ] || [ "$old_cache_hit" -eq -1 ]; then
         echo "UNKOWN - Variables missing in database, please run the check again."
         exit 3
     fi
@@ -56,12 +65,19 @@ last_check=$now
 
     now_s=$(date -d "$now" +%s)
     last_check_s=$(date -d "$last_check" +%s)
-    period=$(( now_s - last_check_s ))
+    period_s=$(( now_s - last_check_s ))
 
-    hitrate=$((((cache_hit - old_cache_hit) * 100) / (total_hit - old_total_hit)))
-    reqpersec=$(bc <<< "scale=1; ($total_hit - $old_total_hit) / $period")
+    period_hit=$((total_hit - old_total_hit))
 
-    echo "Varnish OK : $((total_hit - old_total_hit)) requests in ${period} seconds (cache hitrate ${hitrate}%) | ReqPerSec=$reqpersec CacheHitrate=${hitrate}%"
+    if [ "$period_hit" -gt 0 ]; then
+        hitrate=$((((cache_hit - old_cache_hit) * 100) / period_hit))
+        reqpersec=$(bc <<< "scale=1; ($period_hit / $period_s)")
+    else
+        hitrate=100
+        reqpersec=0
+    fi
+
+    echo "Varnish OK : $period_hit requests in ${period_s} seconds (cache hitrate ${hitrate}%) | ReqPerSec=$reqpersec CacheHitrate=${hitrate}%"
 else
     echo "UNKNOWN : varnishstat returned code $? : $varnishstat"
     exit 3
