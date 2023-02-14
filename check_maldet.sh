4b217b066e2b5a1a445960a87bdca155df188174
Guillaume Subiron <maethor@subiron.org>
2023-02-14 15:49:02 +0100

    Add duration threshold to check_maldet.sh

--- check_maldet.sh
+++ check_maldet.sh
@@ -2,6 +2,8 @@
 
 maldetsessions=/usr/local/maldetect/sess
 
+DURATION_WARN="$1"
+
 if ! [ -e $maldetsessions ] ; then
     echo "UNKNOWN: Maldet is not installed"
     exit 3
@@ -15,17 +17,27 @@ if ! [ -e "$last" ] ; then
 fi
 
 hits=$(grep "TOTAL HITS:" "$last" | grep -o '[[:digit:]]\+' | paste -sd+ | bc)
-duration=$(grep "ELAPSED:" "$last" | awk '{print $2}')
+duration=$(grep "ELAPSED:" "$last" | awk '{print $2}' | sed 's/s//')
+perfdata="duration=${duration}s;$DURATION_WARN;;0;"
 
-if [ "$hits" -eq 0 ] ; then
-    echo "OK: No malware found. Last check took $duration."
-    exit 0
+if [ "$hits" -lt 0 ] ; then
+    echo "UNKNOWN: total hits = $hits"
+    exit 3
 fi
 
 if [ "$hits" -gt 0 ] ; then
-    echo "CRITICAL: $hits malwares found ! Last check took $duration."
+    echo "CRITICAL: $hits malwares found ! | $perfdata" 
     exit 2
 fi
 
-echo "UNKNOWN: total hits = $hits"
-exit 3
+if [ -n "$DURATION_WARN" ] && [ $duration -ge "$DURATION_WARN" ]; then
+    echo "Warning: No malware found but last scan took ${duration} seconds. | $perfdata"
+    exit 1
+fi
+
+if [ "$hits" -eq 0 ] ; then
+    echo "OK: No malware found, last scan took ${duration} seconds. | $perfdata"
+    exit 0
+fi
+
+
