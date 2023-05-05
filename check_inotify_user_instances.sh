6859bb7ada3417a5f8a595dc1306da9516ff6689
Guillaume Subiron <maethor@subiron.org>
2023-05-05 17:16:10 +0200

    Speed up check_inotify_user_instances.sh

--- check_inotify_user_instances.sh
+++ check_inotify_user_instances.sh
@@ -14,32 +14,31 @@ root_count=0
 
 IFS=$'\n'
 
-
 function foo {
     for line in $(find /proc/*/fd/* -type l -lname 'anon_inode:inotify' -print 2>/dev/null | cut -d/ -f3 | sort | uniq -c); do
         count=$(echo "$line" | awk '{print $1}')
-        user=$(echo "$line" | awk '{print $2}' | xargs ps --no-headers -o '%U' -p)
+        user=$(grep Uid /proc/"$(echo "$line" | awk '{print $2}')"/status | awk '{print $3}')
         echo "$count $user"
     done
 }
 
 for i in $(foo | awk '{a[$2] += $1} END{for (i in a) print a[i], i}' | sort -nr); do
     count=$(echo "$i" | sed -e 's/^[ \t]*//' | awk '{print $1}')
-    name=$(echo "$i" | sed -e 's/^[ \t]*//' | awk '{print $2}')
+    uid=$(echo "$i" | sed -e 's/^[ \t]*//' | awk '{print $2}')
+    username=$(id -nu "$uid")
     if [ "$count" -gt "$WARN_THRESHOLD" ] && [ $ret -lt 2 ]; then
-        msg="WARNING - $name user has $count inotify open (Max=$MAX)"
+        msg="WARNING - $username user has $count inotify open (Max=$MAX)"
         ret=1
     fi
     if [ "$count" -gt "$CRIT_THRESHOLD" ]; then
-        msg="CRITICAL - $name user has $count inotify open (Max=$MAX)"
+        msg="CRITICAL - $username user has $count inotify open (Max=$MAX)"
         ret=2
     fi
 
-    if [ "$name" = 'root' ]; then
+    if [ "$username" = 'root' ]; then
         root_count=$count
     fi
 done
 
 echo "$msg | root_inotify_instances=$root_count;$WARN_THRESHOLD;$CRIT_THRESHOLD;0;$MAX;"
 ret=0
-
