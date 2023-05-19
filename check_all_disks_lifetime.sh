e7da20be7ee253644171ff998e39ce0d97f284ee
Guillaume Subiron <maethor@subiron.org>
2023-05-19 11:53:28 +0200

    check_all_disk_lifetime.sh

--- check_all_disks_lifetime.sh
+++ check_all_disks_lifetime.sh
@@ -16,7 +16,7 @@ if ! [[ "$(hostname)" =~ ^(infra|clibre|mt|cz) ]]; then
 fi
 
 # shellcheck disable=SC2010
-for DEVICE in $(ls /sys/block | grep -Ev '^(sr|vd)'); do
+for DEVICE in $(ls /sys/block | grep -Ev '^(sr|vd|fd)'); do
     if [ -L "/sys/block/$DEVICE/device" ]; then
 
     if [[ "$DEVICE" =~ sd ]]; then
