4d432fbc6b58dade47ddad4f5ab89265fa45bece
Guillaume Subiron <maethor@subiron.org>
2023-06-22 08:24:11 +0200

    Debug check_cron_log.sh

--- check_cron_log.sh
+++ check_cron_log.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Do not force-reload around XXhYYm00s because it could skip crons
+while [ "$(date +%S)" -gt 57 ] || [ "$(date +%S)" -lt 3 ]; do sleep 1; done
+
 file_pos=$(wc -l /var/log/cron.log)
 
 /usr/sbin/service cron force-reload
