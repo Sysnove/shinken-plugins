e1bbaad56d9d420ad0e663af0f8b51608e4aca37
Guillaume Subiron <maethor@subiron.org>
2023-08-22 15:03:46 +0200

    check_lsb_services: Ignore NFS when DRBD is secondary

--- check_lsb_services.sh
+++ check_lsb_services.sh
@@ -22,50 +22,53 @@ services="addok addok-wrapper apache2 atd atop bind9 ceph clamav-daemon couchdb 
 down=""
 
 for service in $services; do
-	if [ -f "/etc/init.d/$service" ] || [ -f "/lib/systemd/system/${service}.service" ] || [ -f "/etc/systemd/system/${service}.service" ]; then
-		if [ "$service" = "drbd" ] && [ -x /usr/sbin/gnt-cluster ]; then
-			# Ignore drbd service on Ganeti nodes.
-			continue
-		fi
-
-		service "$service" status >/dev/null || down="$down $service"
-	fi
+    if [ -f "/etc/init.d/$service" ] || [ -f "/lib/systemd/system/${service}.service" ] || [ -f "/etc/systemd/system/${service}.service" ]; then
+        if [ "$service" = "drbd" ] && [ -x /usr/sbin/gnt-cluster ]; then
+            # Ignore drbd service on Ganeti nodes.
+            continue
+        fi
+        if [ "$service" = "nfs-kernel-server" ] && [ -f /proc/drbd ] && grep -q Secondary/Primary /proc/drbd; then
+            # Ignore nfs-kernel-server when first drbd is secondary
+            continue
+        fi
+        service "$service" status >/dev/null || down="$down $service"
+    fi
 done
 
 # Multi-instance postfix, check postfix@-
 if [ -f /lib/systemd/system/postfix@.service ]; then
-	service "postfix@-" status >/dev/null || down="$down postfix@-"
+    service "postfix@-" status >/dev/null || down="$down postfix@-"
 fi
 
 # Postgres special case : 4 means that postgresql-common is installed but not
 # postgresql-server, so it's OK if postgres is not running
 if [ -f /etc/init.d/postgresql ]; then
-	service postgresql status >/dev/null
-	s=$?
-	[ $s -ne 0 ] && [ $s -ne 4 ] && down="$down postgresql"
+    service postgresql status >/dev/null
+    s=$?
+    [ $s -ne 0 ] && [ $s -ne 4 ] && down="$down postgresql"
 fi
 
 # Special case, pureftp can be running but failed.
 # We could make a dedicaded shinken service for this, but we prefer to avoid.
 if [ -f /etc/init.d/pure-ftpd ] || [ -f /etc/init.d/pure-ftpd-mysql ]; then
-	if ! /usr/lib/nagios/plugins/check_ftp -H localhost >/dev/null; then
-		down="$down pure-ftpd"
-	fi
+    if ! /usr/lib/nagios/plugins/check_ftp -H localhost >/dev/null; then
+        down="$down pure-ftpd"
+    fi
 fi
 
 if [ "$down" != "" ]; then
-	echo "WARNING - Services down:$down"
-	exit $STATE_WARNING
+    echo "WARNING - Services down:$down"
+    exit $STATE_WARNING
 else
 
-	# Nginx special case : check if nginx -t is OK
-	if [ -f "/lib/systemd/system/nginx.service" ]; then
-		if ! /usr/sbin/nginx -t >/dev/null 2>&1; then
-			echo "WARNING - Nginx configuration error, please check nginx -t"
-			exit $STATE_WARNING
-		fi
-	fi
+    # Nginx special case : check if nginx -t is OK
+    if [ -f "/lib/systemd/system/nginx.service" ]; then
+        if ! /usr/sbin/nginx -t >/dev/null 2>&1; then
+            echo "WARNING - Nginx configuration error, please check nginx -t"
+            exit $STATE_WARNING
+        fi
+    fi
 
-	echo "OK - All services are running."
-	exit $STATE_OK
+    echo "OK - All services are running."
+    exit $STATE_OK
 fi
