b1dc6202300c4d2c1d250a3f41dd99ae7548e7cb
Julien Rabier <taziden@flexiden.org>
2023-10-06 16:20:45 +0200

    Check all CouchDB replications

--- check_couchdb_replication_wrapper.sh
+++ check_couchdb_replication_wrapper.sh
@@ -5,19 +5,18 @@
 couchdb_version=$(curl -s http://localhost:5984/ | jq -r '.version')
 
 if [ -z "$couchdb_version" ]; then
-    echo "CRITICAL - Could not retrieve CouchDB version."
-    exit 2
+	echo "CRITICAL - Could not retrieve CouchDB version."
+	exit 2
 fi
 
 if [[ "$couchdb_version" = 1.* ]]; then
-    /usr/local/nagios/plugins/check_couchdb_replications.py -r "$1"
-    ret=$?
-    echo "Couchdb $couchdb_version"
-    exit $ret
+	/usr/local/nagios/plugins/check_couchdb_replications.py -r "$1"
+	ret=$?
+	echo "Couchdb $couchdb_version"
+	exit $ret
 else
-    /usr/local/nagios/plugins/check_couchdb2_replications.sh -H localhost -r "$1"
-    ret=$?
-    echo "Couchdb $couchdb_version"
-    exit $ret
+	/usr/local/nagios/plugins/check_couchdb2_replications.sh -H localhost -d
+	ret=$?
+	echo "Couchdb $couchdb_version"
+	exit $ret
 fi
-
