ec4b00b02f31be05e1de27d05adc99fd75a1f657
Alexis Lahouze <alexis@sysnove.fr>
2023-11-10 18:05:51 +0100

    Fix exit code.

--- check_openvpn_server_pkis.sh
+++ check_openvpn_server_pkis.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 for PKI in /etc/openvpn/*-keys; do
-    if ! OUTPUT="$("$(dirname "$0")"/check_pki.sh "${PKI}")"; then
-        EXIT_CODE="$?"
+    OUTPUT="$("$(dirname "$0")"/check_pki.sh "${PKI}")"
+    EXIT_CODE="$?"
+    if [ "$EXIT_CODE" -ne 0 ]; then
         echo "${OUTPUT}" | sed -E 's;^(OK|WARNING|CRITICAL|UNKNOWN): (.*)$;\1: '"${PKI}"' - \2;'
         exit ${EXIT_CODE}
     fi
