ef80e549d5bf4ba2977193594a17097cd492230c
Guillaume Subiron <maethor@subiron.org>
2024-07-04 10:50:50 +0200

    check_backupninja_log.sh

--- check_backupninja_log.sh
+++ check_backupninja_log.sh
@@ -16,9 +16,10 @@ last_line_ts=$(date --date="$last_line_date" +%s)
 now_ts=$(date +%s)
 last_line_date=$(date --date="$last_line_date" +"%Y-%m-%d %H:%M:%S")
 
+resume=$(echo "$last_line" | grep -Eo "[0-9]+ actions run.*")
 
 if [ $((now_ts - last_line_ts)) -gt $((60*60*52)) ]; then
-    echo "CRITICAL - Last backup has finished more than 52 hours from now ($last_line_date)"
+    echo "CRITICAL - Last backup has finished more than 52 hours from now ($last_line_date: $resume)"
     exit 2
 fi
 
@@ -27,18 +28,24 @@ nb_error=$(echo "$last_line" | grep -Eo '[0-9]+ error' | cut -d ' ' -f 1)
 nb_warning=$(echo "$last_line" | grep -Eo '[0-9]+ warning' | cut -d ' ' -f 1)
 
 if [ "$nb_fatal" -gt 0 ]; then
-    echo "CRITICAL - $nb_fatal fatal errors in last backupninja run ($last_line_date)"
+    echo "CRITICAL - $nb_fatal fatal errors in last backupninja run ($last_line_date: $resume)"
+    exit 2
 fi
 
 if [ "$nb_error" -gt 0 ]; then
-    echo "CRITICAL - $nb_error errors in last backupninja run ($last_line_date)"
+    echo "CRITICAL - $nb_error errors in last backupninja run ($last_line_date: $resume)"
+    exit 2
 fi
 
 if [ $((now_ts - last_line_ts)) -gt $((60*60*27)) ]; then
-    echo "WARNING - Last backup has finished more than 27 hours from now ($last_line_date)"
+    echo "WARNING - Last backup has finished more than 27 hours from now ($last_line_date: $resume)"
     exit 1
 fi
 
 if [ "$nb_warning" -gt 0 ]; then
-    echo "WARNING - $nb_warning warnings in last backupninja run ($last_line_date)"
+    echo "WARNING - $nb_warning warnings in last backupninja run ($last_line_date: $resume)"
+    exit 1
 fi
+
+echo "OK - $last_line_date: $resume"
+exit 0
