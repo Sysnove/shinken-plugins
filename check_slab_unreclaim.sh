c559afd1492e6962c5585a9e4727ecaf7f7706e5
Guillaume Subiron <maethor@subiron.org>
2025-02-19 16:49:54 +0100

    check_slab_unreclaim.sh

--- check_slab_unreclaim.sh
+++ check_slab_unreclaim.sh
@@ -8,6 +8,6 @@ slab_unreclaim_m=$((slab_unreclaim_k / 1024))
 
 slab_unreclaim_pct=$(((100 * slab_unreclaim_m) / total_m))
 
-echo "SlabUnreclaim ${slab_unreclaim_m}MB (${slab_unreclaim_pct}%)|sunreclaim=${slab_unreclaim_pct}MB;;;0;$total_m"
+echo "SlabUnreclaim ${slab_unreclaim_m}MB (${slab_unreclaim_pct}%)|sunreclaim=${slab_unreclaim_m}MB;;;0;$total_m"
 exit 0
 
