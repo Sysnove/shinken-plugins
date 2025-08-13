de72726ca550494eff8096f83a896d16f2417c5b
Guillaume Subiron <maethor@subiron.org>
2025-08-13 17:36:50 +0200

    [check_misc] Check IPMI sensors.

--- check_misc.sh
+++ check_misc.sh
@@ -91,12 +91,12 @@ if ! systemd-detect-virt -q; then
     fi
 
     if [ -f $NAGIOS_PLUGINS/check_ipmi_sensor ] && [ -f /usr/sbin/ipmi-sensors ]; then
-        if sudo /usr/sbin/ipmi-sensors > /dev/null 2>&1; then
+        if timeout 5s sudo /usr/sbin/ipmi-sensors > /dev/null 2>&1; then
             cpu_min_freq="$(LC_ALL=C lscpu | grep "min MHz" | awk '{printf "%.0f\n", $NF + 20}')"
             if [ "$(grep 'cpu MHz' /proc/cpuinfo | awk '{sum+=$NF; nb+=1} END {printf "%.0f\n", sum/nb}')" -lt "$cpu_min_freq" ]; then
                 #echo "CRITICAL - CPU is running at 800Mhz. Could be an hardware problem. Please check impi-sensors."
                 #exit 2
-                $NAGIOS_PLUGINS/check_ipmi_sensor --nosel -xT Entity_Presence,Voltage,Physical_Security,Management_Subsystem_Health | cut -d '|' -f 1
+                timeout 5s $NAGIOS_PLUGINS/check_ipmi_sensor --nosel -xT Entity_Presence,Voltage,Physical_Security,Management_Subsystem_Health | cut -d '|' -f 1
             fi
         fi
     fi
