a4c4bd8357257213822c8ec2b39f4303b7167fda
Guillaume Subiron <maethor@subiron.org>
2025-08-22 08:51:19 +0200

    Shellcheck check_mongodb_connection.sh

--- check_mongodb_connection.sh
+++ check_mongodb_connection.sh
@@ -11,20 +11,24 @@ fi
 
 begin=$(date +%s%N | cut -b1-13)
 db_version=$($MONGOCLIENT -u "$USER" -p "$PASSWORD" --eval "db.version()" --quiet)
+ret=$?
 end=$(date +%s%N | cut -b1-13)
 
-if [ $? != 0 ] || [ -z "$db_version" ]; then
+if [ $ret != 0 ] || [ -z "$db_version" ]; then
     echo "UNKNOWN - Could not connect on mongodb"
     exit 3
 fi
 
-duration=$(((end - begin)))
+duration=$((end - begin))
 
 output="MongoDB $db_version is running. Test took $(echo $duration | awk '{printf "%.3f", $1/1000}')s."
 
-if [ "$duration" -gt 2000 ]; then
+if [ "$duration" -gt 4000 ]; then
     echo "CRITICAL - $output"
     exit 2
+elif [ "$duration" -gt 2000 ]; then
+    echo "WARNING - $output"
+    exit 1
 else
     echo "OK - $output"
     exit 0
