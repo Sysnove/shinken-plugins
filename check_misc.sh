b0d7b5ed70a960931d4a7586a04de95c8a10cd9a
Julien Rabier <taziden@flexiden.org>
2025-09-23 10:23:24 +0200

    [check_misc] detect if varnish vcl is missing

--- check_misc.sh
+++ check_misc.sh
@@ -69,15 +69,20 @@ if $TEST_IMAP; then
 fi
 
 (
-set +e
-# shellcheck disable=SC2009
-varnish_vcl=$(ps faux | grep varnishd | grep -Eo '\-f .*\.vcl' | head -n 1 | cut -d ' ' -f 2)
-if [ -f "$varnish_vcl" ]; then
-    if grep -q -E '^ *.probe = {' "$varnish_vcl"; then
-        echo "CRITICAL - Probe should be disabled in $varnish_vcl"
-        exit 2
+    set +e
+    # shellcheck disable=SC2009
+    if pgrep varnishd >/dev/null; then
+        varnish_vcl=$(ps faux | grep varnishd | grep -Eo '\-f .*\.vcl' | head -n 1 | cut -d ' ' -f 2)
+        if [ -f "$varnish_vcl" ]; then
+            if grep -q -E '^ *.probe = {' "$varnish_vcl"; then
+                echo "CRITICAL - Probe should be disabled in $varnish_vcl"
+                exit 2
+            fi
+        else
+            echo "CRITICAL - vcl file does not exist: $varnish_vcl"
+            exit 2
+        fi
     fi
-fi
 )
 
 (
