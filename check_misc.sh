7d1448e17733659d794c07a3d53710a81abeea75
Guillaume Subiron <maethor@subiron.org>
2025-10-22 09:19:31 +0200

    [check_misc.sh] Detect raid with only 1 disk

--- check_misc.sh
+++ check_misc.sh
@@ -88,14 +88,16 @@ fi
 (
 set +e
 # shellcheck disable=SC2013
-for md in $(cat /proc/mdstat | grep -oE '^md[0-9]'); do
-    disks=$(cat /proc/mdstat | grep "^$md" | grep -oE '(hd|sd|nvme).+')
-    nb_disks=$(echo "$disks" | wc -w)
-    if [ "$nb_disks" -lt 2 ]; then
-        echo "CRITICAL - $md RAID is made of $nb_disks disks: $disks"
-        exit 2
-    fi
-done
+if [ -f /proc/mdstat ]; then
+    for md in $(cat /proc/mdstat | grep -oE '^md[0-9]'); do
+        disks=$(cat /proc/mdstat | grep "^$md" | grep -oE '(hd|sd|nvme).+')
+        nb_disks=$(echo "$disks" | wc -w)
+        if [ "$nb_disks" -lt 2 ]; then
+            echo "CRITICAL - $md RAID is made of $nb_disks disks: $disks"
+            exit 2
+        fi
+    done
+fi
 )
 
 (
