8930ab607ade94d3533cd2059a03f04823ae1b11
Guillaume Subiron <maethor@subiron.org>
2025-11-07 11:10:14 +0100

    check_inodes.sh

--- check_inodes.sh
+++ check_inodes.sh
@@ -1,14 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-/usr/bin/df -i | tail -n +2 | while read -r line; do
+THRESHOLD=$1
+
+[ -z "$THRESHOLD" ] && THRESHOLD=10000000
+
+ret_code=0
+
+while read -r line; do
     mount="$(echo "$line" | awk '{print $NF}')"
     inodes="$(echo "$line" | awk '{print $3}')"
 
-    if [ "$inodes" -gt 10000000 ]; then
+    if [ "$inodes" -gt "$THRESHOLD" ]; then
         echo "WARNING : $mount contains $inodes files"
-        exit 2
+        ret_code=1
     fi
-done
+done < <(/usr/bin/df -i | tail -n +2)
 
-echo "OK : No partition over 10M files"
-exit 0
+if [ "$ret_code" -eq 0 ]; then
+    echo "OK : No partition over $THRESHOLD files"
+fi
+
+exit $ret_code
