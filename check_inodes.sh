017e5bf51025a50af1e0c4d218cc74c0abf3197f
Guillaume Subiron <maethor@subiron.org>
2025-11-07 10:51:22 +0100

    check_inodes.sh

---
+++ check_inodes.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+/usr/bin/df -i | tail -n +2 | while read -r line; do
+    mount="$(echo "$line" | awk '{print $NF}')"
+    inodes="$(echo "$line" | awk '{print $3}')"
+
+    if [ "$inodes" -gt 10000000 ]; then
+        echo "WARNING : $mount contains $inodes files"
+        exit 2
+    fi
+done
+
+echo "OK : No partition over 10M files"
+exit 1
