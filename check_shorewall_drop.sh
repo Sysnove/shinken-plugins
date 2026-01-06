e4d85aeba4059aabadda466dd8191ca6399116e3
Guillaume Subiron <maethor@subiron.org>
2026-01-06 07:59:32 +0100

    Check ipset full

--- check_shorewall_drop.sh
+++ check_shorewall_drop.sh
@@ -8,6 +8,20 @@ if which ipset > /dev/null 2>&1; then
         # Yep… I known, I don't care
         dropped=0
     fi
+
+    # Check if there is no full ipset
+    ipsets=$(ipset -L | grep Name | cut -d ' ' -f 2)
+
+    for setname in $ipsets; do
+        if size=$(ipset list "$setname" | grep '^Number of entries' | awk '{print $NF}' 2>/dev/null); then
+            maxelem=$(ipset list "$setname" | grep '^Header' | sed -E 's/^.* maxelem ([0-9]+) .*$/\1/')
+
+            if [[ "$size" -gt "$((maxelem - 1000))" ]] ; then
+                echo "WARNING - ipset $setname is full | dropped=$dropped"
+                exit 1
+            fi
+        fi
+    done
 else
     # Without ipset, use shorewall show dynamic
     if ! which shorewall > /dev/null 2>&1 ; then
@@ -18,7 +32,5 @@ else
     dropped=$(shorewall show dynamic | grep -c DROP)
 fi
 
-echo "OK - $dropped IP address(es) currently dropped by Shorewall | dropped=$dropped"
-
-# Always return OK
+echo "OK - $dropped IP addresses currently dropped by Shorewall | dropped=$dropped"
 exit 0
