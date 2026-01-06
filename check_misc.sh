be0ea750057bd04fca752a6822ca3f35b0ab7138
Guillaume Subiron <maethor@subiron.org>
2026-01-06 14:36:47 +0100

    [check_misc.sh] Check that security.conf snippet is not in Nginx 8080 vhosts

--- check_misc.sh
+++ check_misc.sh
@@ -101,6 +101,19 @@ fi
 )
 
 (
+set +e
+if [ -d /etc/nginx ]; then
+    # Checks for security.conf snippet in Nginx vhosts on port 8080.
+    # Because 403 and 429 returned by security.conf could be cached by Varnish and then returned to everyone.
+    nginx_security_warnings=$(grep -RE '^ *(listen|include snippets/security.conf)' /etc/nginx/sites-enabled | grep 'include snippets' -B 1 | grep ':8080' -A 1 | cut -d ':' -f 1 | uniq)
+    if [ -n "$nginx_security_warnings" ]; then
+        echo "WARNING - You should not use Nginx security.conf snippet in a vhost on port 8080."
+        exit 1
+    fi
+fi
+)
+
+(
 $NAGIOS_PLUGINS/check_ntp_time -H 0.debian.pool.ntp.org | cut -d '|' -f 1
 /usr/local/nagios/plugins/check_shorewall_custom_conf.sh | cut -d '|' -f 1
 /usr/bin/sudo /usr/local/nagios/plugins/check_failover_interfaces.sh | cut -d '|' -f 1
