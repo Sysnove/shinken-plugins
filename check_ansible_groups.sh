5557dd673eeb95d40d42dc40c8f3e9c6225b02ee
Guillaume Subiron <maethor@subiron.org>
2026-01-14 16:29:44 +0100

    [misc] check_ansible_groups

--- check_ansible_groups.sh
+++ check_ansible_groups.sh
@@ -5,8 +5,8 @@
 # magento_servers
 if magento_vhosts=$(grep -Rl '/static.php?resource=' /etc/nginx/sites-enabled); then
     if ! jq -r '.host.ansible_groups[]' /etc/sysnove.json | grep -qw magento_servers; then
-        echo "WARNING: This host should be in magento_servers."
         echo "$magento_vhosts"
+        echo "WARNING: This host should be in magento_servers."
         exit 1
     fi
 fi
