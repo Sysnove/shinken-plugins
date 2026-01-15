b557d259359d8d6fa85f52dc1836053b24978f61
Guillaume Subiron <maethor@subiron.org>
2026-01-15 09:31:30 +0100

    check_ansible_groups.sh mysql_servers_big

--- check_ansible_groups.sh
+++ check_ansible_groups.sh
@@ -12,7 +12,9 @@ if magento_vhosts=$(grep -Rl '/static.php?resource=' /etc/nginx/sites-enabled); 
 fi
 
 # mariadb_servers_big on servers with more than 15Go RAM
-if jq -r '.host.ansible_groups[]' /etc/sysnove.json | grep -qw 'mysql_servers_generic'; then
+# if we configure mysql.
+if [ -f /etc/mysql/conf.d/sysnove.cnf ]; then
+#if jq -r '.host.ansible_groups[]' /etc/sysnove.json | grep -qw 'mysql_servers_generic'; then
     if [ "$(grep MemTotal: /proc/meminfo | awk '{print int($2/1000000)}')" -ge 15 ]; then
         if ! jq -r '.host.ansible_groups[]' /etc/sysnove.json | grep -E '^(mariadb|mysql)_servers_big'; then
             echo "WARNING: This host should be in mysql or mariadb_servers_big."
