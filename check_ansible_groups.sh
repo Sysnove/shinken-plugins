c0f6c0469737f344ceeeab98fa7a1b78763d22e5
Guillaume Subiron <maethor@subiron.org>
2026-01-15 09:23:11 +0100

    check_ansible_groups.sh mysql_servers_big

--- check_ansible_groups.sh
+++ check_ansible_groups.sh
@@ -15,7 +15,7 @@ fi
 if jq -r '.host.ansible_groups[]' /etc/sysnove.json | grep -qw 'mysql_servers_generic'; then
     if [ "$(grep MemTotal: /proc/meminfo | awk '{print int($2/1000000)}')" -ge 15 ]; then
         if ! jq -r '.host.ansible_groups[]' /etc/sysnove.json | grep -E '^(mariadb|mysql)_servers_big'; then
-            echo "WARNING: This host should be in (mariadb|mysql)_servers_big."
+            echo "WARNING: This host should be in mysql or mariadb_servers_big."
             exit 1
         fi
     fi
